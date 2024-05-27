8b0b48663c70417d8e5559f766f0b60d76bc2d0b
Joey Matos Dos Santos <joey.mds@gmail.com>
2024-05-27 21:20:23 +0100

    changed sprites and added animation tree

--- Code/Actors/BasicEnemy.cs
+++ Code/Actors/BasicEnemy.cs
@@ -6,21 +6,31 @@ public partial class BasicEnemy : CharacterBody2D
 	[Export] public float MaxSpeed { get; set; } = 45.0f;
 
 	private HealthComponent _healthComponent;
+	private AnimationTree _animationTree;
 	private Area2D _area2D;
 	
 
 	public override void _Ready()
 	{
 		_healthComponent = GetNode<HealthComponent>("HealthComponent");
+		_animationTree = GetNode<AnimationTree>("AnimationTree");
 	}
 
 	public override void _Process(double delta)
 	{
 		var direction = GetDirectionToPlayer();
 		Velocity = direction * MaxSpeed;
+		
+		if (Velocity == Vector2.Zero)
+			return;
+		
+		var normalizedVelocity = Velocity.Normalized();
+		
+		_animationTree.Set("parameters/Chase/blend_position", normalizedVelocity);
 		MoveAndSlide();
 	}
-
+	
+	
 	private Vector2 GetDirectionToPlayer()
 	{
 		var playerNode = (Node2D)GetTree().GetFirstNodeInGroup("player");
